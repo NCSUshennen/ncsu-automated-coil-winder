2685d05c0a4c65223b0ad9521c3b70f25cc36967
NCSUshennen <shennen@ncsu.edu>
2019-11-01 16:42:49 -0400

    RaspPi Test Serial Files

---
+++ RaspPi/pi_duino_test_led.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import serial
+
+port = "/dev/ttyACM1"
+rate = "9600"
+
+s1 = serial.Serial(port,rate)
+s1.flushInput()
+
+comp_list=["Flash complete\r\n","Hello Pi, this is Arduino MEGA...\r\n"]
+while True:
+    if s1.inWaiting() >0:
+        inputValue = s1.readline()
+        print(inputValue)
+        if inputValue in comp_list:
+            try:
+                n = input("Set Arduino flash times:")
+                s1.write('%d'%n)
+            except:
+                print("Input error, please input a number")
+                s1.write('0')
