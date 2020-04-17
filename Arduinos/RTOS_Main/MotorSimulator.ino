f2e4bfb9a0ba4b7f512053fdc3cf48388096fb64
dwhayduk <56923431+dwhayduk@users.noreply.github.com>
2020-04-16 21:15:13 -0400

    Updated Encoder Simulator
    The encoder simulator more accurately reflects the payout of wire when wound around teeth, and works better with the Raspberry Pi's calculation of wire payout per tooth.

--- Arduinos/RTOS_Main/MotorSimulator.ino
+++ Arduinos/RTOS_Main/MotorSimulator.ino
@@ -19,7 +19,11 @@
 #define LED_CHANGE_RATE 8
 
 // Used for simulating a given elongation
-#define ELONGATION_PERCENT 1
+#define ELONGATION_PERCENT 4.67
+#define WIND_WIDTH 10
+#define WIND_LENGTH 130
+#define WIRE_DIAMETER 1
+
 #define ENCODER_MM_RATE MM_RATE/(1+ELONGATION_PERCENT/100.0)
 
 int simulatorPosition[3] = {0, 0, 0};
@@ -32,6 +36,9 @@ unsigned long int totalSimulatorPulses = 0;
 unsigned long int totalPulsesSentToEncoder = 0;
 unsigned long int pulsesIShouldHaveSent = 0;
 
+unsigned long int xPulsesThisMovement = 0;
+unsigned long int yPulsesThisMovement = 0;
+
 static void MotorSimulator(void* pvParameters)
 {
   digitalWrite(X0_Y0, HIGH);
@@ -41,6 +48,7 @@ static void MotorSimulator(void* pvParameters)
 
   while (1)
   {
+    static enum {S0, S1, S2, S3, S4} nextState = S0;
     enum {X_FORWARD, X_REVERSE, Y_FORWARD, Y_REVERSE, Z_FORWARD, Z_REVERSE} receivedMotorMessage;
     if (xMessageBufferReceive(xMessageBufferM, &receivedMotorMessage, sizeof(receivedMotorMessage), portMAX_DELAY) > 0)
     {
@@ -121,10 +129,122 @@ static void MotorSimulator(void* pvParameters)
           break;
       }
 
-      totalSimulatorPulses++;
+      // Determine whether totalSimulatorPulses should be incremented for purposes of the rotary encoder
+      switch (nextState)
+      {
+        case S0:
+          // Rotary encoder simulator is off (not encoding)
+          if (enableRotaryEncoder && (receivedMotorMessage == X_FORWARD || receivedMotorMessage == X_REVERSE))
+          {
+            nextState = S1;  
+          }
+          else if (enableRotaryEncoder && (receivedMotorMessage == Y_FORWARD || receivedMotorMessage == Y_REVERSE))
+          {
+            nextState = S3;
+          }
+          else
+          {
+            nextState = S0;  
+          }
+          break;
+        case S1:
+          // Start encoded X-movement
+          xPulsesThisMovement = 1;
+          totalSimulatorPulses++;
+          
+          if (enableRotaryEncoder && (receivedMotorMessage == X_FORWARD || receivedMotorMessage == X_REVERSE))
+          {
+            nextState = S2;  
+          }
+          else if (enableRotaryEncoder && (receivedMotorMessage == Y_FORWARD || receivedMotorMessage == Y_REVERSE))
+          {
+            nextState = S3;  
+          }
+          else
+          {
+            nextState = S0;  
+          }
+          break;
+        case S2:
+          // Continue encoded X-Movement
+          xPulsesThisMovement++;
+          if (xPulsesThisMovement <= MM_RATE*WIND_WIDTH)
+          {
+            totalSimulatorPulses++;
+          }
+
+          if (enableRotaryEncoder && (receivedMotorMessage == X_FORWARD || receivedMotorMessage == X_REVERSE))
+          {
+            nextState = S2;  
+          }
+          else if (enableRotaryEncoder && (receivedMotorMessage == Y_FORWARD || receivedMotorMessage == Y_REVERSE))
+          {
+            nextState = S3;  
+          }
+          else
+          {
+            nextState = S0;
+          }
+          break;
+        case S3:
+          // Start encoded Y-movement
+          yPulsesThisMovement = 1;
+          totalSimulatorPulses++;
+          
+          if (enableRotaryEncoder && (receivedMotorMessage == Y_FORWARD || receivedMotorMessage == Y_REVERSE))
+          {
+            nextState = S4;  
+          }
+          else if (enableRotaryEncoder && (receivedMotorMessage == X_FORWARD || receivedMotorMessage == X_REVERSE))
+          {
+            nextState = S1;  
+          }
+          else
+          {
+            nextState = S0;  
+          }
+          break;
+        case S4:
+          // Continue encoded Y-Movement
+          yPulsesThisMovement++;
+          if (yPulsesThisMovement <= MM_RATE*WIND_LENGTH + MM_RATE*WIRE_DIAMETER)
+          {
+            totalSimulatorPulses++;
+          }
+
+          if (enableRotaryEncoder && (receivedMotorMessage == Y_FORWARD || receivedMotorMessage == Y_REVERSE))
+          {
+            nextState = S4;  
+          }
+          else if (enableRotaryEncoder && (receivedMotorMessage == X_FORWARD || receivedMotorMessage == X_REVERSE))
+          {
+            nextState = S1;  
+          }
+          else
+          {
+            nextState = S0;
+          }
+          break;
+        default:
+          if (enableRotaryEncoder && (receivedMotorMessage == X_FORWARD || receivedMotorMessage == X_REVERSE))
+          {
+            nextState = S1;  
+          }
+          else if (enableRotaryEncoder && (receivedMotorMessage == Y_FORWARD || receivedMotorMessage == Y_REVERSE))
+          {
+            nextState = S3;
+          }
+          else
+          {
+            nextState = S0;  
+          }
+          break;  
+      }
+
+      
       pulsesIShouldHaveSent = totalSimulatorPulses/(ENCODER_MM_RATE/3.0); 
       // Based on the total number of motor driver pulses so far, how many pulses should have been sent to pin 2 to simulate the encoder ticking?
-
+      
       if (pulsesIShouldHaveSent > totalPulsesSentToEncoder)
       {
         // Based on the total number of motor driver pulses so far, one more needs to be sent to pin 2 to accurately simulate the rotary encoder
